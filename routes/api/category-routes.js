097e64fc47ef889d1abc594879d5091597a7f0ac
unknown <samtisme@gmail.com>
2023-08-07 18:48:48 -0700

    added get and post route (async) to the category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,20 +13,44 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // add the associated products data to the category
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+      console.log(err);
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+  }
+  catch (err) {
+    res.status(400).json(err);
+    console.log(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
