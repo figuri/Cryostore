b15325449e88813739f04826834e7e62540e1ac5
unknown <samtisme@gmail.com>
2023-08-07 18:31:13 -0700

    add get request for categories and changed function parameters so that it is a async function and I can use await for the findAll method

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,8 +3,16 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
