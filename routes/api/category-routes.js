49a28ed3d1d6720a299e3f5fd483181540a0b525
unknown <samtisme@gmail.com>
2023-08-07 23:44:36 -0700

    added some changes to a few routes/ currently working on testing the functionality of the tag post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,6 +10,7 @@ router.get('/', async (req, res) => {
       include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
+    console/log(res.status(200).json(categoryData));
   } catch (err) {
     res.status(500).json(err);
   }
