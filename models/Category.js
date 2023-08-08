14a0bed6728ce459986b8d87d9edf28017f50c7c
unknown <samtisme@gmail.com>
2023-08-07 18:14:47 -0700

    added model for category

--- models/Category.js
+++ models/Category.js
@@ -5,8 +5,19 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
+  // model for the category table
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
   },
   {
     sequelize,
